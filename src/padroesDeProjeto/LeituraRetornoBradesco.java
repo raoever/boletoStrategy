a4a156c748f23f84c56d6d409c9f065697cb576e
Rene <raoever@gmail.com>
2021-11-17 21:58:19 -0300

    Update Retorno Bradesco.

--- src/padroesDeProjeto/LeituraRetornoBradesco.java
+++ src/padroesDeProjeto/LeituraRetornoBradesco.java
@@ -6,6 +6,7 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,17 +23,15 @@ public class LeituraRetornoBradesco implements LeituraRetorno{
                 Boleto boleto = new Boleto();
                 boleto.setId(Integer.parseInt(vetor[0]));
                 boleto.setCodBanco(vetor[1]);
-                // end::class-start[]
-
-                boleto.setDataVencimento(LocalDate.parse(vetor[2], FORMATO_DATA));
-                boleto.setDataPagamento(LocalDate.parse(vetor[3], FORMATO_DATA).atTime(0, 0, 0));
-
-                boleto.setCpfCliente(vetor[4]);
-                boleto.setValor(Double.parseDouble(vetor[5]));
-                boleto.setMulta(Double.parseDouble(vetor[6]));
-                boleto.setJuros(Double.parseDouble(vetor[7]));
-
-                // tag::class-end[]
+                boleto.setAgencia(vetor[2]);
+                boleto.setContaBancaria(vetor[3]);
+                boleto.setDataVencimento(LocalDate.parse(vetor[4], FORMATO_DATA));
+                boleto.setDataPagamento(LocalDateTime.parse(vetor[5], FORMATO_DATA_HORA));
+                boleto.setCpfCliente(vetor[6]);
+                boleto.setValor(Double.parseDouble(vetor[7]));
+                boleto.setMulta(Double.parseDouble(vetor[8]));
+                boleto.setJuros(Double.parseDouble(vetor[9]));
+                boletos.add(boleto);
                 boletos.add(boleto);
             }
 
--- src/padroesDeProjeto/TesteLeiuraBoleto.java
+++ src/padroesDeProjeto/TesteLeiuraBoleto.java
@@ -2,10 +2,12 @@ package padroesDeProjeto;
 
 public class TesteLeiuraBoleto {
     public static void main(String[] args) {
-        final ProcessarBoletos processador = new ProcessarBoletos(new LeituraRetornoBB());
+//        final ProcessarBoletos processador = new ProcessarBoletos(new LeituraRetornoBB());
+        final ProcessarBoletos processador = new ProcessarBoletos(new LeituraRetornoBradesco());
         String nomeArquivo="";
         try {
-            nomeArquivo = TesteLeiuraBoleto.class.getResource("/banco-brasil-1.csv").getPath();
+//            nomeArquivo = TesteLeiuraBoleto.class.getResource("/banco-brasil-1.csv").getPath();
+            nomeArquivo = TesteLeiuraBoleto.class.getResource("/bradesco-1.csv").getPath();
         } catch (Exception e) {
             System.out.println("Erro ao ler o arquivo");
         }
